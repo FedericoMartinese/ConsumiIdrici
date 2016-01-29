962a61d889645f4965bc7babcfaf8906163b7530
Fede <Fede@05fd7df9-1aff-c842-a7be-554b73f46853>
2016-01-29 15:28:33 +0000

    readFile problem fixed
    git-svn-id: file:///J/repository/fondamenti1_consumiIdrici@27 05fd7df9-1aff-c842-a7be-554b73f46853

--- consumiIdrici/datautility.cpp
+++ consumiIdrici/datautility.cpp
@@ -20,19 +20,9 @@ std::map<QString, consumptionSet> readFile(QString fileName) {
         QTextStream in(&inputFile);
         int c = 1;
         while (!in.atEnd()) {
-            std::string line = in.readLine().toStdString();
-
-            QString params[3];
-            //QStringList params = line.split(',');
-
-            try {
-                int v1,v2;
-                v1 = line.find(',');
-                v2 = line.find(',', v1+1);
-                params[0] = QString::fromStdString(line.substr(0,v1));
-                params[1] = QString::fromStdString(line.substr(v1+1, v2-v1-1));
-                params[2] = QString::fromStdString(line.substr(v2+1));
-            } catch (...) { //parametri non suddivisi correttamente o in numero errato
+            QString line = in.readLine();
+            QStringList params = line.split(',');
+            if (params.length() != 3) { //parametri non suddivisi correttamente o in numero errato
                 QMessageBox msg(QMessageBox::Critical, "Consumi idrici", "Errore nella lettura dei dati. Formato dati errati (riga " + QString::number(c) + ")." , QMessageBox::Abort | QMessageBox::Ignore);
                 if (msg.exec() == QMessageBox::Abort) {
                     inputFile.close();
@@ -44,15 +34,14 @@ std::map<QString, consumptionSet> readFile(QString fileName) {
             //converte la stringa in formato datetime. sono presenti " all'inizio e alla fine
             //toUTC è necessario per gestire ora legale/solare (ad esempio 29-03-2015 02:00)
             //che viene poi convertita in CET dalla funzione UTCtoDayLightSavTime
-            QString clientID = params[2];
             bool ok;
             try {
                 consumption cons(UTCtoDayLightSavTime(QDateTime::fromString(params[0], "\"yyyy-MM-dd HH:mm:ss\"").toUTC()), params[1].toDouble(&ok));
 
-                if (!ok) throw "Cast failed";
+                if (!ok || params[2].length() == 0) throw "Cast failed";
 
                 //aggiunge consumi al cliente (se non è presente il cliente nella mappa lo crea e posiziona in ordine)
-                clients[clientID].insert(cons);
+                clients[params[2]].insert(cons);
             } catch (...) {
                 QMessageBox msg(QMessageBox::Critical, "Consumi idrici", "Errore nella lettura dei dati. Dati errati (riga " + QString::number(c) + ")." , QMessageBox::Abort | QMessageBox::Ignore);
                 if (msg.exec() == QMessageBox::Abort) {
